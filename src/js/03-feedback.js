57b11b2c5b70e9bf6639242944e2761c9e973a5a
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-08 15:39:53 +0300

    hw-08 task-3 feedback, заповнення форми

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,14 @@
+const refs = {
+	form: document.querySelector('.feedback-form'),
+	textarea: document.querySelector('.feedback-form textarea'),
+};
+
+// refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextereaInput);
+
+function onTextereaInput(evt) {
+	const value = evt.target.value;
+
+	console.log(value);
+}
 
