f6ecbc5e303a12389036f518712e5901eb206009
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-10 21:09:52 +0300

    hw-08 video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const playCurrentTime = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+	localStorage.setItem(playCurrentTime, seconds);
+ }
+ 
+ player.setCurrentTime(localStorage.getItem(playCurrentTime));
