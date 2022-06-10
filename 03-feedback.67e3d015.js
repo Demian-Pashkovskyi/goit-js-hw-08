699f6e0e3cc0c0bdf44327c9fd50d262d4740f29
Demian-Pashkovskyi <95377702+Demian-Pashkovskyi@users.noreply.github.com>
2022-06-10 08:30:40 +0000

    Deploying to gh-pages from @ Demian-Pashkovskyi/goit-js-hw-08@b024f3fa0d9010927080eeb58b33aa22f660c54e 🚀

--- 03-feedback.67e3d015.js
+++
@@ -1,2 +0,0 @@
-({form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")}).textarea.addEventListener("input",(function(e){const t=e.target.value;console.log(t)}));
-//# sourceMappingURL=03-feedback.67e3d015.js.map
--- 03-feedback.7e3cf59c.js
+++
@@ -1,2 +0,0 @@
-(document.querySelector(".feedback-form"),document.querySelector(".feedback-form textarea")).addEventListener("input",(function(e){var t=e.target.value;console.log(t)}));
-//# sourceMappingURL=03-feedback.7e3cf59c.js.map
