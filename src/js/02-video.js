740b34df8110a92be7c9985054ff0f81bb7b61fb
Demian Pashkovskyi <skyangely4@gmail.com>
2022-06-12 10:31:18 +0300

    hw-js-08 правка

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,11 +5,18 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 const playCurrentTime = 'videoplayer-current-time';
+const getTime = localStorage.getItem(playCurrentTime);
 
-player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(Number(getTime));
 
-function onPlay({ seconds }) {
-	localStorage.setItem(playCurrentTime, seconds);
- }
- 
- player.setCurrentTime(localStorage.getItem(playCurrentTime));
+player.on('timeupdate', throttle(event => {
+   setLocalStoreg(event);
+}, 250),
+);
+
+const setLocalStoreg = event => {
+	localStorage.setItem(playCurrentTime, event.seconds.toString());
+	if (event.seconds === event.duration) {
+		localStorage.clear();
+	}
+};
